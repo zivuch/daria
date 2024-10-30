e3db6530921d2bea3eda19764dd155cf6b2a33b8
Daria Ingatiuk <Ignatyuk.dariaa@gmail.com>
2024-10-30 11:41:29 +0200

    fixing deployment 5

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,29 +6,20 @@ function App() {
   const [data, setData] = useState('');
   const [name, setName] = useState('');
   // const BASE_URL:string = 'http://localhost:3001';
-  const BASE_URL = process.env.REACT_APP_BASE_URL;
-  const send = async () => {
-   if(name){
-    try {
-          const response = await fetch(`${BASE_URL}/api/${name}`);
-          const data = await response.json();
-          console.log("data", data);
-          setData(data);
-    } catch (error) {
-      console.log(error);
-      
-    }
-
-    // fetch(`${BASE_URL}/api/${name}`)
-    //   .then((res) => {
-    //     console.log("res", res);
-    //     return res.json();
-    //   })
-    //   .then((data) => {
-    //     console.log("data", data);
-    //     setData(data.message);
-    //   })
-    //   .catch((e) => console.log(e));
+  const BASE_URL = import.meta.env.VITE_APP_BASE_URL;
+  
+  const send = () => {
+  if(name){
+    fetch(`/api/${name}`)
+      .then((res) => {
+        console.log("res", res);
+        return res.json();
+      })
+      .then((data) => {
+        console.log("data", data);
+        setData(data.message);
+      })
+      .catch((e) => console.log(e));
     }
   }
 
